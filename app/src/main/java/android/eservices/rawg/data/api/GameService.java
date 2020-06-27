85a6737a49d91c47570d0c9c88c523ad0a4a1125
Massiaux Alexis <alexis.massiaux@worldline.com>
2020-06-27 15:13:12 +0200

    feat: addition of a service to get a specific game from the id

--- app/src/main/java/android/eservices/rawg/data/api/GameService.java
+++ app/src/main/java/android/eservices/rawg/data/api/GameService.java
@@ -1,13 +1,18 @@
 package android.eservices.rawg.data.api;
 
+import android.eservices.rawg.data.api.model.Game;
 import android.eservices.rawg.data.api.model.GameSearchResponse;
 
 import io.reactivex.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface GameService {
 
     @GET("games")
     Single<GameSearchResponse> getGamesSearchResponses(@Query("search") String keywords);
+
+    @GET("games/{id}")
+    Single<Game> getGame(@Path("id") String id);
 }
